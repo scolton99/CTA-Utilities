e8e35aef4d7c04aeb0f58b2b08309cd39ce7b030
Spencer Colton <me@scolton.tech>
2020-07-12 15:01:10 -0500

    Mocked up Redis since MemoryStore is wayyyy too expensive lol

--- src/server/util/Redis.ts
+++ src/server/util/Redis.ts
@@ -1,8 +1,31 @@
-import redis from "redis";
-import { promisify } from "util";
+// import redis from "redis";
+// import { promisify } from "util";
+//
+// const redis_client = redis.createClient();
+//
+// export const get = promisify(redis_client.get).bind(redis_client);
+// export const set = promisify(redis_client.set).bind(redis_client);
+// export const expire = promisify(redis_client.expire).bind(redis_client);
 
-const redis_client = redis.createClient();
+const store: { [key: string]: string } = {};
 
-export const get = promisify(redis_client.get).bind(redis_client);
-export const set = promisify(redis_client.set).bind(redis_client);
-export const expire = promisify(redis_client.expire).bind(redis_client);
+const expirations: { [key: string]: number } = {};
+
+export const get = async (key: string): Promise<string> => {
+    if (!store[key]) return null;
+
+    const time = Date.now();
+    if (expirations[key] && expirations[key] > time) {
+        return store[key];
+    } else {
+        return null;
+    }
+};
+
+export const set = async (key: string, value: string): Promise<void> => {
+    store[key] = value;
+};
+
+export const expire = async (key: string, seconds: number): Promise<void> => {
+    expirations[key] = Date.now() + 1000 * seconds;
+};
