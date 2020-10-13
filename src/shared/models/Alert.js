2230debf351367cdde4e7c8adadc0b990a042963
Spencer Colton <me@scolton.tech>
2020-10-13 08:16:53 -0500

    Remove extraneous JS files

--- src/shared/models/Alert.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Alert = /** @class */ (function () {
-    function Alert() {
-    }
-    return Alert;
-}());
-exports.default = Alert;
-//# sourceMappingURL=Alert.js.map
